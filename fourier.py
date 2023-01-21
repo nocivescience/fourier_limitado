03893f6d29122cc82a87325c3b023c270959a2cf
noceive <nocivescience@gmail.com>
2023-01-21 00:12:51 -0300

    incluidos los circulos

--- fourier.py
+++ fourier.py
@@ -1,7 +1,7 @@
 from manim import *
 class FourierManim(Scene):
     CONFIG={
-        'wait_time': 122,
+        'wait_time': 12,
         'vector_config':{
             'max_tip_length_to_length_ratio':.25,
             'tip_length': 0.15,
@@ -10,15 +10,21 @@ class FourierManim(Scene):
         'vector_time':ValueTracker(0),
         'center_point':ORIGIN,
         'n_vectors':10,
+        'circle_config':{
+            'fill_opacity':0,
+            'stroke_width':.2
+        }
     }
     def construct(self):
-        vector= self.get_rotating_vectors()
-        self.add(vector)
+        vectors= self.get_rotating_vectors()
+        circles= self.get_circles(vectors)
+        self.add(vectors,circles)
         self.wait(self.CONFIG['wait_time'])
     def get_freqs(self):
-        return list(np.linspace(1,2,32))
+        n=self.CONFIG['n_vectors']
+        return list(range(n//2,-n//2,-1))
     def get_coefficients(self):
-        return list(np.linspace(.1*5,.2*5,32))
+        return list(complex(1).real for _ in range(self.CONFIG['n_vectors']))
     def get_rotating_vectors(self,freqs=None, coeffs=None):
         vectors=VGroup()
         self.center_tracker=VectorizedPoint(self.CONFIG['center_point'])
@@ -49,3 +55,19 @@ class FourierManim(Scene):
         vector.set_length(vector.coeff)
         vector.rotate((time+dt)*vector.freq,about_point=vector.get_start())
         vector.shift(vector.center_func()-vector.get_start())
+    def get_circle(self,vector,color=None):
+        if color is None:
+            color=RED
+        circle=Circle(color=color,**self.CONFIG['circle_config'])
+        circle.func_center=vector.get_start
+        circle.func_radius=vector.get_length
+        circle.add_updater(self.update_circle)
+        return circle
+    def get_circles(self,vectors):
+        return VGroup(*[
+            self.get_circle(vector) for vector in vectors
+        ])
+    def update_circle(self,circle):
+        circle.set_width(2*circle.func_radius())
+        circle.move_to(circle.func_center())
+        return circle
