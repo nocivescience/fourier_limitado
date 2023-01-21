81bab4eecc8086880e3dd9689cd5d16206d6edbb
noceive <nocivescience@gmail.com>
2023-01-21 17:59:09 -0300

    removere el component_y

--- fourier.py
+++ fourier.py
@@ -1,4 +1,7 @@
 from manim import *
+import itertools as it
+import operator as op 
+import functools as ft
 class FourierManim(Scene):
     CONFIG={
         'wait_time': 12,
@@ -13,18 +16,22 @@ class FourierManim(Scene):
         'circle_config':{
             'fill_opacity':0,
             'stroke_width':.2
-        }
+        },
+        'low_factor':0.5,
     }
     def construct(self):
         vectors= self.get_rotating_vectors()
         circles= self.get_circles(vectors)
-        self.add(vectors,circles)
+        paths=self.get_drawn_path(vectors)
+        copies_wave=self.get_y_component_wave(vectors)
+        self.add(vectors,circles,paths,copies_wave)
         self.wait(self.CONFIG['wait_time'])
     def get_freqs(self):
         n=self.CONFIG['n_vectors']
-        return list(range(n//2,-n//2,-1))
+        return list(np.linspace(0.3,1,n))
     def get_coefficients(self):
-        return list(complex(1).real for _ in range(self.CONFIG['n_vectors']))
+        n=self.CONFIG['n_vectors']
+        return [complex(.3) for _ in range(n)]
     def get_rotating_vectors(self,freqs=None, coeffs=None):
         vectors=VGroup()
         self.center_tracker=VectorizedPoint(self.CONFIG['center_point'])
@@ -52,7 +59,7 @@ class FourierManim(Scene):
         return vector
     def update_vector(self,vector,dt):
         time=self.CONFIG['vector_time'].get_value()
-        vector.set_length(vector.coeff)
+        vector.set_length(vector.coeff.real)
         vector.rotate((time+dt)*vector.freq,about_point=vector.get_start())
         vector.shift(vector.center_func()-vector.get_start())
     def get_circle(self,vector,color=None):
@@ -71,3 +78,63 @@ class FourierManim(Scene):
         circle.set_width(2*circle.func_radius())
         circle.move_to(circle.func_center())
         return circle
+    def get_vector_sum_path(self,vectors,color=YELLOW):
+        coeffs=[v.coeff for v in vectors]
+        freqs=[v.freq for v in vectors]
+        center=vectors[0].get_start()
+        path=ParametricFunction(
+            lambda t: center+ ft.reduce(op.add,[complex_to_R3(coeff*np.exp(TAU*1j*freq*t))
+                for coeff,freq in zip(coeffs, freqs)                                                  
+            ]),
+            t_range=[0,1]
+        )
+        return path
+    def get_drawn_path(self,vectors,**kwargs):
+        path=self.get_vector_sum_path(vectors)
+        broken_path=CurvesAsSubmobjects(path)
+        broken_path.curr_time=0
+        start, end=[0,1]
+        def update_path(path,dt):
+            alpha=self.CONFIG['vector_time'].get_value()
+            n_curves=len(path)
+            for a, sp in zip(np.linspace(0,1,n_curves), path):
+                b= (alpha-a)
+                if b<0:
+                    width=0
+                else:
+                    width=2*interpolate(
+                        start,end, (1-(b%1))
+                    )
+                sp.set_stroke(width=width)
+            path.curr_time+=dt
+            return path
+        broken_path.set_color(YELLOW_B)
+        broken_path.add_updater(update_path)
+        return broken_path
+    def get_y_component_wave(self,vectors,left_x=1,color=PINK, n_copies=2,right_shift_rate=5):
+        path=self.get_vector_sum_path(vectors)
+        wave=ParametricFunction(
+            lambda t: op.add(
+                right_shift_rate*t*LEFT,
+                path.function(t)[1]*UP
+            ),
+            t_range=[0,1]  #poner path.t_range[0], poner path.t_range[1]
+        )
+        wave_copies=VGroup(*[
+            wave.copy()
+            for x in range(n_copies)
+        ]).arrange(RIGHT,buff=0)
+        top_point=wave_copies.get_top()
+        wave.creation=Create(wave,run_time=self.CONFIG['low_factor'], rate_Func=linear)
+        cycle_animation(wave.creation)
+        wave.add_updater(
+            lambda m: m.shift((m.get_left()[0]-left_x)*LEFT)
+        )
+        def update_wave_copies(wcs):
+            index=int(wave.creation.total_time*self.CONFIG['low_factor'])
+            wcs[:index].match_style(wave)
+            wcs[index:].set_stroke(width=0)
+            wcs.next_to(wave,RIGHT,buff=0)
+            wcs.align_to(top_point, UP)
+        wave_copies.add_updater(update_wave_copies)
+        return VGroup(wave,wave_copies)
